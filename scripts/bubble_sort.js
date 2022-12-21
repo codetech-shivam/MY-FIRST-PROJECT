919bb38e13d50f251478a28211f05f816c9526dc
shivakant6894 <116540818+codetech-shivam@users.noreply.github.com>
2022-12-21 12:37:37 +0530

    21/12/2022

--- scripts/bubble_sort.js
+++ scripts/bubble_sort.js
@@ -19,6 +19,7 @@ function Bubble()
 
     for(var i=0;i<array_size-1;i++)
     {
+        //var t=0;
         for(var j=0;j<array_size-i-1;j++)
         {
             div_update(divs[j],div_sizes[j],"yellow");//Color update
@@ -27,7 +28,7 @@ function Bubble()
             {
                 div_update(divs[j],div_sizes[j], "red");//Color update
                 div_update(divs[j+1],div_sizes[j+1], "red");//Color update
-
+                //t=t+1;
                 var temp=div_sizes[j];
                 div_sizes[j]=div_sizes[j+1];
                 div_sizes[j+1]=temp;
@@ -38,6 +39,10 @@ function Bubble()
             div_update(divs[j],div_sizes[j], "blue");//Color updat
         }
         div_update(divs[j],div_sizes[j], "green");//Color update
+        // if(t==0){
+        //     break;
+        // }
+        
     }
     div_update(divs[0],div_sizes[0], "green");//Color update
 
